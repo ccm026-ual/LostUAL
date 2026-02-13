31c27be90e1bd2126f1245a4e8279a57b2177d1f
ccm026-ual <ccm026@inlumine.ual.es>
2026-02-13 21:42:14 +0100

    Rework de la página de moderación

---
+++ LostUAL.Web/Shared/Labels.cs
@@ -0,0 +1,32 @@
+﻿using LostUAL.Contracts.Claims;
+using LostUAL.Contracts.Posts;
+using LostUAL.Contracts.Reports;
+
+public static class Labels
+{
+    public static string ReportStatusLabel(ReportStatus s) => s switch
+    {
+        ReportStatus.Open => "Abierto",
+        ReportStatus.Dismissed => "Desestimado",
+        ReportStatus.ActionTaken => "Acción tomada",
+        _ => s.ToString()
+    };
+
+    public static string ClaimStatusLabel(ClaimStatus s) => s switch
+    {
+        ClaimStatus.Pending => "Pendiente",
+        ClaimStatus.Accepted => "Aceptada",
+        ClaimStatus.Rejected => "Rechazada",
+        ClaimStatus.Withdrawn => "Retirada",
+        _ => s.ToString()
+    };
+
+    public static string PostStatusLabel(PostStatus s) => s switch
+    {
+        PostStatus.Open => "Abierto",
+        PostStatus.InClaim => "En reclamación",
+        PostStatus.Closed => "Cerrado",
+        PostStatus.Resolved => "Resuelto",
+        _ => s.ToString()
+    };
+}
