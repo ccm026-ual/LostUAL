701b1b56ea231ba959ac212d51debf0436d5ea3b
ccm026-ual <ccm026@inlumine.ual.es>
2026-02-13 20:12:46 +0100

    Modo  oscuro añadido

---
+++ LostUAL.Web/wwwroot/js/theme.js
@@ -0,0 +1,7 @@
+﻿window.theme = {
+    get: () => localStorage.getItem("theme") || "light",
+    set: (t) => {
+        document.documentElement.setAttribute("data-bs-theme", t);
+        localStorage.setItem("theme", t);
+    }
+};
